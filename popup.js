9b42d670c65d4397cfb0d3082991362837d9939c
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-06-22 08:42:02 +0700

    feat: get location input from form in popup

--- popup.js
+++ popup.js
@@ -1,6 +1,16 @@
+// placeholder leaflet.js map
 const map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// retrieve location input whenever new one submitted
+const locationInput = document.getElementsByClassName("location-form-input")[0];
+locationInput.addEventListener("change", () => {
+    const locationInputValue = locationInput.value;
+    console.log(locationInputValue);
+
+    // TODO: whenever location changes, run appropriate checks and add to list
+})
