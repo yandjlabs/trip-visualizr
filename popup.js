9386a34010767245676cc45860c79262b74317b8
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-06-22 09:01:21 +0700

    feat: draw markers for coordinates in list

--- popup.js
+++ popup.js
@@ -1,11 +1,28 @@
-// placeholder leaflet.js map
-const map = L.map('map').setView([51.505, -0.09], 13);
+// CODE FOR MAP
+// init leaflet.js map
+const map = L.map('map').setView([33.750746, -84.391830], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' // DO NOT REMOVE OR WE WILL GET SUED
 }).addTo(map);
 
+// dummy coordinates list
+COORDINATES_LIST = [
+    [33.7575, -84.3897], // aquarium
+    [33.762573, -84.392586], // coke company hq
+    [33.7262, -84.3685] // zoo
+]
+
+// draw pins on map for every coordinate in list
+function drawCoordinates() {
+    for (coordinate of COORDINATES_LIST) {
+        L.marker(coordinate).addTo(map);
+    }
+}
+drawCoordinates(); // call this function on build, and whenever list updates
+
+// CODE FOR LOCATION INPUT PROCESSING
 // retrieve location input whenever new one submitted
 const locationInput = document.getElementsByClassName("location-form-input")[0];
 locationInput.addEventListener("change", () => {
