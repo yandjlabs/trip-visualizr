50245b3f3ef3ebf03549b0dd890b126296cd9857
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-06-25 17:09:13 +0800

    feat: add html, css file handling to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,44 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {
-    background: './src/background.js',
-    content: './src/content.js',
-    popup: './src/popup.js'
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: 'production',
+    entry: {
+        background: './src/background.js',
+        content: './src/content.js',
+        popup: './src/popup.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            // {
+            //     test: /\.html$/,
+            //     use: 'html-loader'
+            // }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].bundle.css'
+        }),
+        new HtmlWebpackPlugin({
+            template: 'static/popup.html',
+            filename: 'popup.bundle.html',
+            chunks: ['popup']
+        }),
+    ]
 };
