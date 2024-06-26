940623ff72b2b98b3856c537cde1f0dc9a1d681d
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-06-26 08:26:51 +0800

    fix: removed unused utils

--- popup.js
+++ popup.js
@@ -58,26 +58,3 @@ function processLocationAddress(address) {
 
     // return coords, address in one object
 }
-
-// UTILS (DIRECTLY COPIED AND PASTED FROM STACKOVERFLOW)
-function ParseDMS(input) {
-    var parts = input.split(/[^\d\w\.]+/);
-    var lat = ConvertDMSToDD(parts[0], parts[2], parts[3], parts[4]);
-    var lng = ConvertDMSToDD(parts[5], parts[7], parts[8], parts[9]);
-
-    return {
-        Latitude: lat,
-        Longitude: lng,
-        Position: lat + ',' + lng
-    }
-}
-
-
-function ConvertDMSToDD(degrees, minutes, seconds, direction) {
-    var dd = Number(degrees) + Number(minutes) / 60 + Number(seconds) / (60 * 60);
-
-    if (direction == "S" || direction == "W") {
-        dd = dd * -1;
-    } // Don't do anything for N or E
-    return dd;
-}
